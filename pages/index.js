60c6c275497f28e530fe6004e4a9bafc91b0c904
Rajarshi Mandal <rajarshim5@gmail.com>
2023-06-19 21:23:13 +0530

    Added mui pagination

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,61 @@
-import Pagination from '../components/pagination';
-import { useState } from 'react';
+import { Pagination } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const totalPages = 10; 
+  const [totalPages, setTotalPages] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+  const [coinData, setCoinData] = useState([]);
 
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-    fetchData(pageNumber);
+  const handlePageChange = (e, value) => {
+    setCurrentPage(value);
+    fetchData(value);
   };
 
-  const fetchData = (pageNumber) => {
-    ///// for axios call 
+  const fetchData = async (pageNumber) => {
+    const data = await axios.get(
+      `http://localhost:3010/coins?page=${pageNumber}`
+    );
+    !totalPages && setTotalPages(data.data.totalPages);
+    console.log(data.data);
+    setCoinData(data.data.coins);
   };
+  useEffect(() => {
+    fetchData(1);
+  }, []);
 
   return (
-    <div className='flex flex-col'>
-      <h3 className='flex justify-center'>Top Crypto Exchanges</h3>
-      <h5 className='flex justify-center'>Compare all 190 Top Crypto Exchanges. The list is ranked by top trading volume</h5>
-
-     {Array.from({ length: totalPages }, (_, index) => (
-         <div className= "flex justify-center px-4 py-2 hover:bg-gray-300">
-           <li className="list-none	 mx-4"
-            key={index}>
-           {index+1}
-          </li>
-          <li className="list-none mx-4	"> ... </li>
-         </div>
-        ))}
+    <div className="flex flex-col">
+      <h3 className="flex justify-center">Top Crypto Exchanges</h3>
+      <h5 className="flex justify-center">
+        Compare all 190 Top Crypto Exchanges. The list is ranked by top trading
+        volume
+      </h5>
+      <ul className="flex justify-between px-4 py-2">
+      <li className="list-none	 mx-4" >Exchanges</li>
+      <li className="list-none	 mx-4" >24 hour trade volume</li>
+      </ul>
+      {coinData.map((e, i) => (
+        <div className="flex justify-between px-4 py-2 hover:bg-gray-300 " key = {i}>
+          <div className="flex justify-center px-4 py-2" >
+            <li className="list-none	 mx-4" >{i + 1}</li>
+            {e.url && <img src= {e.url} className="list-none	 mx-4" />}
+            {e.name && <li className="list-none	 mx-4" >{e.name}</li>}
+          </div>
+          {e.volume_1day_usd && <li className="list-none mx-4	">$ {e.volume_1day_usd}</li>}
+        </div>
+      ))}
       <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
+        count={totalPages}
+        variant="outlined"
+        color="primary"
+        className={"pagination"}
+        style={{
+          marginBottom: "30px",
+        }}
+        page={currentPage}
+        onChange={handlePageChange}
       />
     </div>
   );
-};
+}
